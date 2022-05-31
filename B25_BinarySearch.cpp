1850c951adb90833c8fb74828314cf81064bc01a
Hoàng Phương <101681993+HoangPhuong22@users.noreply.github.com>
2022-05-31 10:58:00 +0700

    Update B25_BinarySearch.cpp

--- B25_BinarySearch.cpp
+++ B25_BinarySearch.cpp
@@ -7,8 +7,8 @@ bool check(int a[] , int l , int r , int x)
     if(l > r) return false;
     int m = (l + r) / 2;
     if(a[m] == x) return true;
-    else if(a[m] > x) return check(a , l , m - 1 , x);
-    else return check(a , m + 1 , r , x);
+    else if(a[m] > x) return check(a , m + 1, r, x);
+    else return check(a , l , m - 1 , x);
 }
 int main()
 {
